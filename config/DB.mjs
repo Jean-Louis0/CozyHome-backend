f85e4829c543170538d7263f734675211e2e2e8a
Jean-Louis <girishakajeanlouis0@gmail.com>
2023-09-27 19:37:41 +0200

    DB connection config

--- config/DB.mjs
+++ config/DB.mjs
@@ -1,29 +1,34 @@
-import pkg from 'pg'
-import dotenv from 'dotenv'
+import pkg from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
-dotenv.config()
 // Create a PostgreSQL connection pool
-const { Client } = pkg
+const { Client } = pkg;
 
-let clientconfig = {
+const clientconfig = {
     database: 'CozyHome',
     host: 'localhost',
     port: 5432,
     user: 'girishakapsql',
     password: process.env.PASSWORD_POSTGRESQL
-}
+};
 
-const conn = new Client(clientconfig)
+// Create the PostgreSQL connection URL
+const connectionString = `postgres://${clientconfig.user}:${clientconfig.password}@${clientconfig.host}:${clientconfig.port}/${clientconfig.database}`;
+
+// Create a new PostgreSQL client with the connection URL
+const conn = new Client({
+    connectionString: connectionString
+});
 
 // Connect to the PostgreSQL database
 conn.connect()
-  .then(() => {
-    console.log('Connected to the database');
-  })
-  .catch((err) => {
-    console.error('Error connecting to the database', err);
-  });
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database', err);
+    });
 
-
-export default conn
+export default conn;
